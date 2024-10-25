e3f08166fcd8016729e50bedae8c402e8eeba580
rorychatt <pavel.rinne@gmail.com>
2024-10-25 20:53:45 +0200

    feat: add logic to output value

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,10 @@ import "./style.css";
 const addButton = document.getElementById("addButton");
 const input1 = document.getElementById("input1")! as HTMLInputElement;
 const input2 = document.getElementById("input2")! as HTMLInputElement;
+const result = document.getElementById("result")!;
 
 if (addButton) {
   addButton.addEventListener("click", () => {
-    console.log(parseFloat(input1.value) + parseFloat(input2.value));
+    result.innerText = (parseFloat(input1.value) + parseFloat(input2.value)).toString();
   });
 }
