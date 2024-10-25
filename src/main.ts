174b74d1ba1d105e0f7da34671afbe2d04c3205d
rorychatt <pavel.rinne@gmail.com>
2024-10-25 20:33:10 +0200

    feat: add click event listener to btn

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
 import './style.css';
+
+const addButton = document.getElementById('addButton');
+
+if(addButton) {
+    addButton.addEventListener('click', () => {
+        console.log("AAAAAA");
+    });
+}
