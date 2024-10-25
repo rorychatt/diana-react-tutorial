6391a272db5490e680c3aa4f4a932b176efb8b70
mirukurun <diana.horbyk@gmail.com>
2024-10-25 20:49:46 +0200

    feat: add const + parseFloat

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
-import './style.css';
+import "./style.css";
 
-const addButton = document.getElementById('addButton');
+const addButton = document.getElementById("addButton");
+const input1 = document.getElementById("input1")! as HTMLInputElement;
+const input2 = document.getElementById("input2")! as HTMLInputElement;
 
-if(addButton) {
-    addButton.addEventListener('click', () => {
-        console.log("AAAAAA");
-    });
+if (addButton) {
+  addButton.addEventListener("click", () => {
+    console.log(parseFloat(input1.value) + parseFloat(input2.value));
+  });
 }
