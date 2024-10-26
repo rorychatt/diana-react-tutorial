e4e8f504516034bb948593265e7c7e6805a7acc1
mirukurun <diana.horbyk@gmail.com>
2024-10-27 00:19:44 +0200

    add: button + *design ))

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import "./style.css";
 
 const addButton = document.getElementById("addButton");
+const subtractButton = document.getElementById("subtractButton");
 const input1 = document.getElementById("input1")! as HTMLInputElement;
 const input2 = document.getElementById("input2")! as HTMLInputElement;
 const result = document.getElementById("result")!;
@@ -8,5 +9,10 @@ const result = document.getElementById("result")!;
 if (addButton) {
   addButton.addEventListener("click", () => {
     result.innerText = (parseFloat(input1.value) + parseFloat(input2.value)).toString();
+  });
+}
+if (subtractButton) {
+  subtractButton.addEventListener("click", () => {
+      result.innerText = (parseFloat(input1.value) - parseFloat(input2.value)).toString();
   });
 }
